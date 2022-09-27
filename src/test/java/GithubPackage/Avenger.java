3378773325d5519cfe136f4f74c1e381465cc1d6
User <User@WINDOWS-3U6Q86V>
2022-09-27 16:30:56 +0530

    new update

---
+++ src/test/java/GithubPackage/Avenger.java
@@ -0,0 +1,16 @@
+package GithubPackage;
+
+import org.testng.annotations.Test;
+
+public class Avenger {
+	@Test 
+	public void aveng(){
+		System.out.println("*****HULK******");
+	}
+	@Test 
+	public void aveng1(){
+		System.out.println("*****CAPT AMERICA*****");
+	}
+	
+
+}
