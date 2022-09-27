ce2d9a4f457f6eb8053de1e8ddeeefc49bbf0783
User <User@WINDOWS-3U6Q86V>
2022-09-27 15:13:06 +0530

    updated

---
+++ src/test/java/GithubPackage/Demo.java
@@ -0,0 +1,14 @@
+package GithubPackage;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+
+	
+	
+		System.out.println("-----tested-----");
+	}
+
+}
