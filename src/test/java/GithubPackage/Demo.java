126d643cfbe6648c8456406605dddb59748f6a21
User <User@WINDOWS-3U6Q86V>
2022-09-27 15:24:37 +0530

    update done....

--- src/test/java/GithubPackage/Demo.java
+++ src/test/java/GithubPackage/Demo.java
@@ -1,14 +1,9 @@
 package GithubPackage;
 
-import org.testng.annotations.Test;
-
 public class Demo {
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
-
-	
-	
-		System.out.println("-----tested-----");
+		System.out.println("-----demo00000----");
 	}
 
 }
