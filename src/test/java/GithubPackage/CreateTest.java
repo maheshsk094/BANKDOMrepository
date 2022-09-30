367d8721a20894ebf6896188a7e4820fd26bac04
VSBeelagi <vijayalaxmibeelagi10@gmail.com>
2022-09-30 11:38:47 +0530

    Build Creation

--- src/test/java/GithubPackage/CreateTest.java
+++ src/test/java/GithubPackage/CreateTest.java
@@ -10,6 +10,7 @@ public class CreateTest {
 		System.out.println("---created1--");
 		System.out.println("---created2--");
 		System.out.println("---created3--");
+		System.out.println("--Build Creation for Jenkins--");
 	}
 
 	
@@ -21,5 +22,6 @@ public class CreateTest {
 		System.out.println("---created11--");
 		System.out.println("---created22--");
 		System.out.println("---created33--");
+		
 	}
 }
